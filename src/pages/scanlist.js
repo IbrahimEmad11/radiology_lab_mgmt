d0ab43a5ff5df7bdc07f1347c7f75f3e56734cc3
omar mohamed <omarmohhameed828@gmial.com>
2023-06-16 15:23:29 +0300

    first version of scanlist window

--- src/pages/scanlist.js
+++ src/pages/scanlist.js
@@ -25,7 +25,82 @@ export default function Scanlist() {
       {/*    )*/}
       {/*  })*/}
       {/*}*/}
-      <p className="text=3xl">this is the home page</p>
+      {/* adding search bar */}
+      <div className="p-10">
+        <label
+          for="username"
+          className="block text-lg text-gray-900 dark:text-gray-300"
+        >
+          patient name
+        </label>
+        <div className="flex ">
+        <input
+          type="text"
+          placeholder="Enter Patient Name"
+          className="block  mt-2 mr-5 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300 h-12"
+        />
+        <button class="px-6 py-2 mt-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80 h-12">
+          search
+        </button>
+        </div>
+      </div>
+      {/* end of search bar */}
+
+      {/* start of scan table */}
+      <div className="block  mt-2 mr-5 w-full pl-10 pr-10">
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <h2 className="text-lg font-medium text-gray-800 dark:text-white">Recent Scans</h2>
+        <div className="flex items-center mt-4 gap-x-3">
+          <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clipPath="url(#clip0_3098_154395)">
+                <path d="M13.3333 13.3332L9.99997 9.9999M9.99997 9.9999L6.66663 13.3332M9.99997 9.9999V17.4999M16.9916 15.3249C17.8044 14.8818 18.4465 14.1806 18.8165 13.3321C19.1866 12.4835 19.2635 11.5359 19.0351 10.6388C18.8068 9.7417 18.2862 8.94616 17.5555 8.37778C16.8248 7.80939 15.9257 7.50052 15 7.4999H13.95C13.6977 6.52427 13.2276 5.61852 12.5749 4.85073C11.9222 4.08295 11.104 3.47311 10.1817 3.06708C9.25943 2.66104 8.25709 2.46937 7.25006 2.50647C6.24304 2.54358 5.25752 2.80849 4.36761 3.28129C3.47771 3.7541 2.70656 4.42249 2.11215 5.23622C1.51774 6.04996 1.11554 6.98785 0.935783 7.9794C0.756025 8.97095 0.803388 9.99035 1.07431 10.961C1.34523 11.9316 1.83267 12.8281 2.49997 13.5832" stroke="currentColor" strokeWidth="1.67" strokeLinecap="round" strokeLinejoin="round" />
+              </g>
+            </svg>
+            <span>New Scan</span>
+          </button>
+        </div>
+      </div>
+      <table className="mt-6 bg-white dark:bg-gray-800 min-w-full border divide-y dark:border-gray-700">
+        <thead>
+          <tr>
+            <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Patient Name
+            </th>
+            <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Age
+            </th>
+            <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              type
+            </th>
+            <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Scan
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Add table rows dynamically based on your data */}
+          <tr>
+            <td className="px-4 py-3 text-sm font-normal text-gray-900 whitespace-nowrap">
+              omar mohamed
+            </td>
+            <td className="px-4 py-3 text-sm font-normal text-gray-900 whitespace-nowrap">
+              21
+            </td>
+            <td className="px-4 py-3 text-sm font-normal text-gray-900 whitespace-nowrap">
+              ay 7aga
+            </td>
+            <td className="px-4 py-3 text-sm font-normal text-gray-900 whitespace-nowrap">
+              {/* Add actions for each row */}
+              <button className="text-blue-500 hover:text-blue-600">show  scan</button>
+            </td>
+          </tr>
+          {/* Add more table rows as needed */}
+        </tbody>
+      </table>
+    </div>
+
+      {/* end of scan table */}
     </div>
   )
 }
