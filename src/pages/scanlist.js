0ebe7c4a7ae255fd9f966bb5b35dc017b316ed45
Amir Hesham <amirhesham65@gmail.com>
2023-06-17 20:33:43 +0300

    Update Scanlist view

--- src/pages/scanlist.js
+++ src/pages/scanlist.js
@@ -1,141 +1,190 @@
 import Sidebar from "@/componenets/Sidebar";
 import Link from "next/link"
-
-const scans = [
-  {
-    patient_name: "Amir",
-    age: 14,
-    type: "resonance",
-    scan_id: 1,
-  },
-  {
-    patient_name: "mona",
-    age: 19,
-    type: "x-ray",
-    scan_id: 2,
-  },
-  {
-    patient_name: "omar",
-    age: 21,
-    type: "ct",
-    scan_id: 3,
-  },
-  {
-    patient_name: "mahmoud",
-    age: 21,
-    type: "mri",
-    scan_id: 4,
-  },
-  // {
-  //   patient_name: "mazen",
-  //   age: 29,
-  //   type: 'M',
-  //   scan_id: 5
-  // },
-];
-
 export default function Scanlist() {
   return (
     <div className="flex">
       <Sidebar currentLinkId={0} />
-      <div className="ml-72 mr-8 container mx-auto p-10">
-        <h1 className="text-2xl">Scans List</h1>
-        {/* adding search bar */}
-        <div className="my-10">
-          <label
-            htmlFor="username"
-            className="block text-lg text-gray-900 dark:text-gray-300"
-          >
-            Search
-          </label>
-          <div className="flex">
-            <input
-              type="text"
-              placeholder="Enter Patient Name"
-              className="block  mt-2 mr-5 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300 h-12"
-            />
-            <button className="px-6 py-2 mt-2 font-medium tracking-wide text-white capitalize transition-colors text-sm duration-200 bg-blue-500 rounded-lg sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
-              search
+      <section className="ml-72 mr-8 container mx-auto my-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <div>
+            <div className="flex items-center gap-x-3">
+              <h2 className="text-lg font-medium text-gray-800 dark:text-white">Scans List</h2>
+
+              <span
+                className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">14 Scans</span>
+            </div>
+
+            <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">A list of all the scans history in your radiology lab</p>
+          </div>
+
+          <div className="flex items-center mt-4 gap-x-3">
+            <button
+              className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clipPath="url(#clip0_3098_154395)">
+                  <path
+                    d="M13.3333 13.3332L9.99997 9.9999M9.99997 9.9999L6.66663 13.3332M9.99997 9.9999V17.4999M16.9916 15.3249C17.8044 14.8818 18.4465 14.1806 18.8165 13.3321C19.1866 12.4835 19.2635 11.5359 19.0351 10.6388C18.8068 9.7417 18.2862 8.94616 17.5555 8.37778C16.8248 7.80939 15.9257 7.50052 15 7.4999H13.95C13.6977 6.52427 13.2276 5.61852 12.5749 4.85073C11.9222 4.08295 11.104 3.47311 10.1817 3.06708C9.25943 2.66104 8.25709 2.46937 7.25006 2.50647C6.24304 2.54358 5.25752 2.80849 4.36761 3.28129C3.47771 3.7541 2.70656 4.42249 2.11215 5.23622C1.51774 6.04996 1.11554 6.98785 0.935783 7.9794C0.756025 8.97095 0.803388 9.99035 1.07431 10.961C1.34523 11.9316 1.83267 12.8281 2.49997 13.5832"
+                    stroke="currentColor" strokeWidth="1.67" strokeLinecap="round" strokeLinejoin="round"/>
+                </g>
+                <defs>
+                  <clipPath id="clip0_3098_154395">
+                    <rect width="20" height="20" fill="white"/>
+                  </clipPath>
+                </defs>
+              </svg>
+
+              <span>Import Data</span>
+            </button>
+
+            <button
+              className="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                   stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round"
+                      d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
+              </svg>
+
+              <span>Add New Scan</span>
             </button>
           </div>
         </div>
-        {/* end of search bar */}
 
-        {/* start of scan table */}
-        <div className="block mt-2 mr-5 w-full">
-          <div className="sm:flex sm:items-center sm:justify-between">
-            <h2 className="text-lg font-medium text-gray-800 dark:text-white">
-              Recent Scans
-            </h2>
-            <div className="flex items-center mt-4 gap-x-3">
-              <Link href="/newscan" className="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
-                <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g clipPath="url(#clip0_3098_154395)">
-                    <path
-                      d="M13.3333 13.3332L9.99997 9.9999M9.99997 9.9999L6.66663 13.3332M9.99997 9.9999V17.4999M16.9916 15.3249C17.8044 14.8818 18.4465 14.1806 18.8165 13.3321C19.1866 12.4835 19.2635 11.5359 19.0351 10.6388C18.8068 9.7417 18.2862 8.94616 17.5555 8.37778C16.8248 7.80939 15.9257 7.50052 15 7.4999H13.95C13.6977 6.52427 13.2276 5.61852 12.5749 4.85073C11.9222 4.08295 11.104 3.47311 10.1817 3.06708C9.25943 2.66104 8.25709 2.46937 7.25006 2.50647C6.24304 2.54358 5.25752 2.80849 4.36761 3.28129C3.47771 3.7541 2.70656 4.42249 2.11215 5.23622C1.51774 6.04996 1.11554 6.98785 0.935783 7.9794C0.756025 8.97095 0.803388 9.99035 1.07431 10.961C1.34523 11.9316 1.83267 12.8281 2.49997 13.5832"
-                      stroke="currentColor"
-                      strokeWidth="1.67"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </g>
-                </svg>
-                <span>New Scan</span>
-              </Link>
-            </div>
+        <div className="mt-6 md:flex md:items-center md:justify-between">
+          <div
+            className="inline-flex overflow-hidden bg-white border divide-x rounded-lg dark:bg-gray-900 rtl:flex-row-reverse dark:border-gray-700 dark:divide-gray-700">
+            <button
+              className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 bg-gray-100 sm:text-sm dark:bg-gray-800 dark:text-gray-300">
+              View all
+            </button>
+
+            <button
+              className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
+              Completed
+            </button>
+
+            <button
+              className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
+              In Progress
+            </button>
           </div>
-          <table className="mt-6 bg-white dark:bg-gray-800 min-w-full border divide-y dark:border-gray-700">
-            <thead>
-            <tr>
-              <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Patient Name
-              </th>
-              <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Age
-              </th>
-              <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Type
-              </th>
-              <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Scan
-              </th>
-            </tr>
-            </thead>
-            <tbody>
-            {scans.map((scan) => (
-              <tr key={scan.scan_id}>
-                <td className="px-4 py-3 text-sm font-normal text-gray-900 dark:text-gray-50 whitespace-nowrap">
-                  {scan.patient_name}
-                </td>
-                <td className="px-4 py-3 text-sm font-normal text-gray-900 dark:text-gray-50 whitespace-nowrap">
-                  {scan.age}
-                </td>
-                <td className="px-4 py-3 text-sm font-normal text-gray-900 dark:text-gray-50 whitespace-nowrap">
-                  {scan.type}
-                </td>
-                <td className="px-4 py-3 text-sm font-normal text-gray-900 dark:text-gray-50 whitespace-nowrap">
-                  {/* Add actions for each row */}
-                  <Link
-                    href={`/viewscan`}
-                    className="text-blue-500 hover:text-blue-600"
-                  >
-                    View Scan
-                  </Link>
-                </td>
-              </tr>
-            ))}
-            </tbody>
-          </table>
+
+          <div className="relative flex items-center mt-4 md:mt-0">
+            <span className="absolute">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                     stroke="currentColor" className="w-5 h-5 mx-3 text-gray-400 dark:text-gray-600">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                          d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
+                </svg>
+            </span>
+
+            <input type="text" placeholder="Search"
+                   className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+          </div>
         </div>
 
-        {/* end of scan table */}
-      </div>
+        <div className="flex flex-col mt-6">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                  <thead className="bg-gray-50 dark:bg-gray-800">
+                  <tr>
+                    <th scope="col"
+                        className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                      Patient
+                    </th>
+
+                    <th scope="col"
+                        className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                      Scan Status
+                    </th>
+
+                    <th scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                      Scan Type
+                    </th>
+
+
+                    <th scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                      Assigned Doctor
+                    </th>
+
+                    <th scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                      Scan Date
+                    </th>
+
+                  </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                  <tr>
+                    <td className="px-4 py-4 text-sm font-medium whitespace-nowrap">
+                      <div>
+                        <h2 className="font-medium text-gray-800 dark:text-white ">Amir Hesham</h2>
+                        <p className="text-sm font-normal text-gray-600 dark:text-gray-400">21 Years</p>
+                      </div>
+                    </td>
+                    <td className="px-12 py-4 text-sm font-medium whitespace-nowrap">
+                      <div
+                        className="inline px-3 py-1 text-sm font-normal rounded-full text-emerald-500 gap-x-2 bg-emerald-100/60 dark:bg-gray-800">
+                        Completed
+                      </div>
+                    </td>
+                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                      <div>
+                        <h4 className="text-gray-700 dark:text-gray-200">Magnetic Resonance Imaging</h4>
+                        <p className="text-gray-500 dark:text-gray-400">Back and Shoulders</p>
+                      </div>
+                    </td>
+                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                      <div>
+                        <h4 className="text-gray-700 dark:text-gray-200">Dr. Metwally Ahmed</h4>
+                      </div>
+                    </td>
+                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                      <p className="text-gray-500 dark:text-gray-300">17 Jun 2023</p>
+                    </td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-6 sm:flex sm:items-center sm:justify-between ">
+          <div className="text-sm text-gray-500 dark:text-gray-400">
+            Page <span className="font-medium text-gray-700 dark:text-gray-100">1 of 10</span>
+          </div>
+
+          <div className="flex items-center mt-4 gap-x-4 sm:mt-0">
+            <a href="#"
+               className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                   stroke="currentColor" className="w-5 h-5 rtl:-scale-x-100">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"/>
+              </svg>
+
+              <span>
+                    previous
+                </span>
+            </a>
+
+            <a href="#"
+               className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
+                <span>
+                    Next
+                </span>
+
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                   stroke="currentColor" className="w-5 h-5 rtl:-scale-x-100">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
