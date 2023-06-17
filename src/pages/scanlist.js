a599ed7e1c4e688c250ebe3579083dc1701b2049
Amir Hesham <amirhesham65@gmail.com>
2023-06-17 22:41:57 +0300

    Add data insertion in Add New Scan page

--- src/pages/scanlist.js
+++ src/pages/scanlist.js
@@ -25,7 +25,7 @@ export default function Scanlist({ scansData }) {
               <h2 className="text-lg font-medium text-gray-800 dark:text-white">Scans List</h2>
 
               <span
-                className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">14 Scans</span>
+                className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">{`${scansData.length} Scans`}</span>
             </div>
 
             <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">A list of all the scans history in your radiology lab</p>
@@ -50,7 +50,8 @@ export default function Scanlist({ scansData }) {
               <span>Import Data</span>
             </button>
 
-            <button
+            <Link
+              href="/newscan"
               className="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                    stroke="currentColor" className="w-5 h-5">
@@ -59,7 +60,7 @@ export default function Scanlist({ scansData }) {
               </svg>
 
               <span>Add New Scan</span>
-            </button>
+            </Link>
           </div>
         </div>
 
@@ -179,7 +180,7 @@ export default function Scanlist({ scansData }) {
 
         <div className="mt-6 sm:flex sm:items-center sm:justify-between ">
           <div className="text-sm text-gray-500 dark:text-gray-400">
-            Page <span className="font-medium text-gray-700 dark:text-gray-100">1 of 10</span>
+            Page <span className="font-medium text-gray-700 dark:text-gray-100">1 of 1</span>
           </div>
 
           <div className="flex items-center mt-4 gap-x-4 sm:mt-0">
