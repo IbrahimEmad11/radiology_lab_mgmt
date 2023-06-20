dbe565853d5eb644182f45e5ade963ee396df605
Amir Hesham <amirhesham65@gmail.com>
2023-06-20 19:11:15 +0300

    Add crappy auth

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,35 @@
 import Head from "next/head"
 import Link from "next/link"
-
+import {useEffect} from "react"
+import { useForm } from "react-hook-form"
+import {supabase} from "@/managers/supabase";
+import {useRouter} from "next/router";
 export default function Login() {
+  const { register, handleSubmit, formState: { errors }, reset,watch } = useForm()
+
+  const router = useRouter()
+
+  const onSubmit = async data => {
+    const { email, password } = data
+    if (email.trim() && password.trim()) {
+      const { data, error } = await supabase
+        .from("doctors")
+        .select()
+        .eq("email", email.trim())
+        .maybeSingle()
+      if (!data) {
+        alert("A user with this email doesn't exist!")
+      } else {
+        if (data["password"] === password.trim()) {
+          await router.push("/dashboard")
+        } else {
+          alert("Password entered is not correct!")
+        }
+      }
+
+    }
+  }
+
   return (
     <>
      <Head>
@@ -19,26 +47,25 @@ export default function Login() {
 
             <p className="mt-1 px-3 text-center text-sm text-gray-500 dark:text-gray-400">Use your doctor or technician account to access dashboard</p>
 
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="w-full mt-4">
                 <input
                   className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:text-white dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
-                  type="email" placeholder="Email Address" aria-label="Email Address"/>
+                  {...register("email", { required: true })} type="email" placeholder="Email Address" aria-label="Email Address"/>
               </div>
 
               <div className="w-full mt-4">
                 <input
                   className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:text-white dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
-                  type="password" placeholder="Password" aria-label="Password"/>
+                  {...register("password", { required: true })} type="password" placeholder="Password" aria-label="Password"/>
               </div>
 
               <div className="flex items-center justify-between mt-4">
-                <a href="#" className="text-sm text-gray-600 dark:text-gray-200 hover:text-gray-500">Forget Password?</a>
-
-                <Link href="/scanlist"
-                  className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                  Sign In
-                </Link>
+                <input
+                  type="submit"
+                  className="mx-auto text-center px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                  value="Sign In"
+                />
               </div>
             </form>
           </div>
@@ -46,7 +73,7 @@ export default function Login() {
           <div className="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
             <span className="text-sm text-gray-600 dark:text-gray-200">Don't have an account? </span>
 
-            <a href="#" className="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline">Register</a>
+            <a href="#" className="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline">Request one.</a>
           </div>
         </div>
       </main>
