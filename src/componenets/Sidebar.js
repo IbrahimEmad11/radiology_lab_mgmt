e29e4ed802e9550dc3baf25fdec4e5b982a5b1e5
Amir Hesham <amirhesham65@gmail.com>
2023-06-20 15:02:21 +0300

    Add doctors list

--- src/componenets/Sidebar.js
+++ src/componenets/Sidebar.js
@@ -32,7 +32,7 @@ const navigationLinks = [
 
   {
     id: 2,
-    name: "Admins",
+    name: "Doctors",
     icon: (
       <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
@@ -42,7 +42,7 @@ const navigationLinks = [
               strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
       </svg>
     ),
-    href: "/admins"
+    href: "/doctors"
   },
 ]
 
